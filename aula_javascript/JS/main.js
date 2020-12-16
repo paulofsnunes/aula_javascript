4e9f57cee3705e4b130a56e3cf134e3df42c0e08
Paulo Franklin <75373785+paulofsnunes@users.noreply.github.com>
2020-12-16 11:13:15 -0300

    Add files via upload

---
+++ aula_javascript/JS/main.js
@@ -0,0 +1,102 @@
+
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigrado por clicar";
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://www.google.com/webhp?hl=pt-BR&ictx=2&sa=X&ved=0ahUKEwiKkvzb1sjtAhWKDrkGHfb5Dt0QPQgI");
+    //window.location.href = "redirecionar";
+}
+
+function trocar(){
+    alert("trocar texto")
+}
+
+//function soma(n1, n2){
+    //return n1 + n2;
+//}
+
+
+/*
+var validar = 0;
+
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual a sua idade?");
+validaIdade(idade)
+console.log(validar);
+*/
+/*
+var d= new Date();
+alert(d.getDay()+1);
+alert(d.getHours()+1);
+alert(d.getMinutes()+1);
+*/
+/*
+var count;
+for (count=0; count <= 5; count++){
+    alert(count);
+}
+*/
+
+
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual a sua idade?");
+var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+
+//var frutas = [{nome: "maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+
+/*
+var fruta = {nome: "maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+//var nome = "Paulo Nunes";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2)
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toUpperCase());
+//alert(frase.replace("Japão", "Brasil"));
